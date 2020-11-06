0f91aed972364ea19426c7077d133faa7c2961c8
EricFormar <66625345+EricFormar@users.noreply.github.com>
2020-11-06 12:07:55 -0300

    modificaciones en app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+// git init (para inicializar un repositorio remoto)
+
+// (crear o modificar archvios dentro de la carpeta donde se inicializó el repositorio)
+
+// git status
+
+/* On branch master
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+        modified:   app.js
+
+no changes added to commit (use "git add" and/or "git commit -a") */
+
+
+// git add . (para agregar TODO a la 'caja')
+
+// git status
+
+
+
+// git commit -m "mensaje del commit" (esto es para etiquetar y enviar la 'caja')
